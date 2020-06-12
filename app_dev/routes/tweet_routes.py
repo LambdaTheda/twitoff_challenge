877dac8a79bcd1e5f9b48c10275ed6257e2b77a4
Cortesha <crd3cc04@gmail.com>
2020-06-11 19:31:24 -0500

    Pulling data from database with request

--- app_dev/routes/tweet_routes.py
+++ app_dev/routes/tweet_routes.py
@@ -2,22 +2,30 @@
 
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
+from app_dev.models import db, Tweet, parse_records
+
 tweet_routes = Blueprint("tweet_routes", __name__)
 
 @tweet_routes.route("/tweets.json")
 def list_tweets():
-    tweets = [
-        {"id": 1, "cory_ken": "Motivate"},
-        {"id": 2, "kyle_jen": "New Makeup"}
-    ]
+    #tweets = [
+    #    {"id": 1, "cory_ken": "Motivate"},
+    #    {"id": 2, "kyle_jen": "New Makeup"}
+    #]
+
+    tweet_records = Tweet.query.all()
+    print(tweet_records)
+    tweets = parse_records(tweet_records)
     return jsonify(tweets)
 
 @tweet_routes.route("/tweets")
 def list_tweets_from_twitter():
-    tweets = [
-        {"id": 1, "cory_ken": "Motivate"},
-        {"id": 2, "kyle_jen": "New Makeup"}
-    ]
+    #tweets = [
+    #    {"id": 1, "cory_ken": "Motivate"},
+    #    {"id": 2, "kyle_jen": "New Makeup"}
+    #]
+    tweet_records = Tweet.query.all()
+    tweets = parse_records(tweet_records)
     return render_template("tweets.html", message="What are they saying on Twitter", tweets=tweets)
 
 
@@ -28,9 +36,15 @@ def new_tweet():
 @tweet_routes.route("/tweets/create", methods=["POST"])
 def create_tweet():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
+
+
+    new_tweet = Tweet(tweet=request.form["tweet"], user=request.form["user_name"])
+    db.session.add(new_tweet)
+    db.session.commit()# todo: store in database
+
+
     return jsonify({
-        "message": "TWEET CREATED OK (TODO)",
+        "message": "TWEET CREATED OK",
         "tweet": dict(request.form)
     })
     #flash(f"Book '{new_book.title}' created successfully!", "success")
