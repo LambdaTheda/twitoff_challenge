62b28742e77849618f019db0427f9733465dd254
Cortesha <crd3cc04@gmail.com>
2020-06-12 19:56:29 -0500

    Example Classifier

--- app_dev/routes/twitter_routes.py
+++ app_dev/routes/twitter_routes.py
@@ -47,5 +47,4 @@ def get_user(screen_name=None):
         db.session.add(db_tweet)
         counter+=1
         db.session.commit()
-        return "OK"
-    #return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
+    return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
